ce25bcb7c58f9472be404bee50bb325208fdbfe9
changhu <o52tiger@gmail.com>
2014-12-07 16:01:23 +0800

    初始化数据

--- test_game/init.js
+++ test_game/init.js
@@ -5,9 +5,11 @@ mongoose.connect('mongodb://' + settings.host + '/' + settings.db);
 
 require('./models/questioncategory.js');
 require('./models/questionstore.js');
+require('./models/question.js');
 
 
 var QuestionCategory = mongoose.model('QuestionCategory');
+var Question = mongoose.model('Question');
 
 for(var i = 0 ;i < 10; i++){
 
@@ -15,6 +17,7 @@ for(var i = 0 ;i < 10; i++){
     qc.qcid = '' + i;
     qc.pid = '' + 0;
     qc.title = i + '类题目';
+    qc.isParent = true;
 
     qc.save(function(err){
         if(err) throw err;
@@ -25,10 +28,32 @@ for(var i = 0 ;i < 10; i++){
         qcc.qcid = i+'-'+j;
         qcc.pid = '' + i;
         qcc.title =  i + '-' + j + '类题目';
+        qcc.isParent = false;
 
         qcc.save(function(err){
             if(err) throw err;
         })
+
+    }
+
+    for(var k = 0 ; k < 10; k++){
+        var q  = new Question();
+
+        q.qid = '' + k;
+        q.qsid = '' + i;
+        q.title = '第' + i +  '题:XXXXXXXXXXXXXXXXXXX';
+        q.opts = {
+            a:'AAAAAAAAAAAAAAAAAAA',
+            b:'BBBBBBBBBBBBBBBBB',
+            c:'CCCCCCCCCCCCCCCCC'
+        };
+        q.answer = 'a';
+        q.score = 1;
+
+        q.save(function(err){
+            if(err) throw err;
+        })
+
     }
 }
 
