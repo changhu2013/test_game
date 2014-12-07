b9be877c1d18dab7a2a58e7e1ae66d50e0f465c8
changhu <changhu2013@users.noreply.github.com>
2014-12-07 12:23:52 +0800

    提交上传  乱码未解决

--- test_game/routes/admin.js
+++ test_game/routes/admin.js
@@ -3,17 +3,19 @@
  */
 
 var express = require('express');
+var mongoose = require('mongoose');
 var url = require('url');
 var querystring = require('querystring');
 var path = require('path');
 var fs = require('fs');
 
+//上传相关的中间键
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
-
 var Settings = require('../models/settings.js');
 
+var encoding = require('encoding');// 解决乱码
 var router = express.Router();
 
 router.get('/', function(req, res){
@@ -43,10 +45,33 @@ router.get('/importusers', function(req, res){
 });
 
 router.post('/importusers', multipartMiddleware, function(req, res){
-    //var upfile = req.files.file;
     console.log('import users');
+    req.setEncoding('utf-8');//请求编码
     //TODO : 实现读取文件内容将用户信息存入数据库
+    var temp_path = req.files.file.path;
+    if (temp_path) {
+        fs.readFile(temp_path, 'utf-8', function(err, content) {
 
+            console.log(encoding.convert(content, 'gb2312'));
+
+            var users = content.split('\r\n');
+            for(var i= 1,len=users.length;i<len;i++){
+                var userArr = users[i].split(',');
+                var User = mongoose.model('user');
+                var user = new User();
+                user.sid = userArr[0];
+                user.name = userArr[1];
+                user.job = userArr[2];
+                user.save(function (err) {
+                    if(err) throw err;
+                });
+            }
+            console.log('上传成功');
+
+            // 删除临时文件
+            fs.unlink(temp_path);
+        });
+    }
     res.redirect('/admin#/importusers');
 });
 
