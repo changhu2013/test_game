56a3f0b917323add3e77894efdb0de7bd6582017
changhu <changhu2013@users.noreply.github.com>
2014-12-07 12:14:16 +0800

    提交上传  乱码未解决

--- test_game/models/user.js
+++ test_game/models/user.js
@@ -1,37 +1,12 @@
-var mongodb = require('./db.js');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-function User(cfg){
-    this.sid = cfg.sid;
-    this.name = cfg.name;
-    //职位
-    this.job = cfg.job;
-    //积分
-    this.score = cfg.score;
-    //挑战次数
-    this.battles = cfg.battles;
-}
+var User = new Schema({
+    sid: String,
+    name: String,
+    job: String,
+    score: Number,
+    battles: Number
+});
 
-User.get = function get(sid, callback){
-    mongodb.open(function(err, db){
-        if(err){
-            return callback(err);
-        }
-        db.collection('users', function(err, collection){
-            if(err){
-                mongodb.close();
-                return callback(err);
-            }
-            collection.findOne({sid : sid}, function(err, doc){
-                mongodb.close();
-                if(doc){
-                    var user = new User(doc);
-                    callback(err, user);
-                }else {
-                    callback(err, null);
-                }
-            });
-        });
-    });
-};
-
-module.exports = User;
+mongoose.model('user', User);
