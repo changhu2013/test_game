c2e4b53451338a0243b86602d41441291ed79ac6
changhu <o52tiger@gmail.com>
2014-12-08 11:04:01 +0800

    生成测试数据

--- test_game/init.js
+++ test_game/init.js
@@ -3,98 +3,227 @@ var mongoose = require('mongoose');
 //mongoose
 mongoose.connect('mongodb://' + settings.host + '/' + settings.db);
 
+require('./models/user.js');
 require('./models/questioncategory.js');
 require('./models/questionstore.js');
 require('./models/question.js');
 require('./models/battle.js');
 
 
+var User = mongoose.model('User');
 var QuestionCategory = mongoose.model('QuestionCategory');
 var QuestionStore = mongoose.model('QuestionStore');
 var Question = mongoose.model('Question');
-
 var Battle = mongoose.model('Battle');
 
-/*
-for(var i = 0 ;i < 10; i++){
+//删除之前数据
+User.collection.drop();
+QuestionCategory.collection.drop();
+QuestionStore.collection.drop();
+Question.collection.drop();
+Battle.collection.drop();
 
-    var qc = new QuestionCategory();
-    qc.qcid = '' + i;
-    qc.pid = '' + 0;
-    qc.title = i + '类题目';
-    qc.isParent = true;
+//测试用户
+var user = new User({
+    sid: '1',
+    name: '张三',
+    job: '程序员',
+    score: 100,
+    battles: 78
+});
+user.save(function (err) {
+    if (err) throw err;
+});
 
-    qc.save(function(err){
-        if(err) throw err;
-    })
+//测试题目分类
+new QuestionCategory({
+    qcid: '10',
+    pid: '0',
+    title: '财务审计部',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
 
-    for(var j = 0; j < 5; j++){
-        var qcc = new QuestionCategory();
-        qcc.qcid = i+'-'+j;
-        qcc.pid = '' + i;
-        qcc.title =  i + '-' + j + '类题目 该题目集下无题目';
-        qcc.isParent = false;
+new QuestionCategory({
+    qcid: '11',
+    pid: '0',
+    title: '市场客服部',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
 
-        qcc.save(function(err){
-            if(err) throw err;
-        })
+new QuestionCategory({
+    qcid: '12',
+    pid: '0',
+    title: '车队运营部',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+
+new QuestionCategory({
+    qcid: '13',
+    pid: '0',
+    title: '安全监管部',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '20',
+    pid: '10',
+    title: '财务会计', isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '21',
+    pid: '10',
+    title: '财务会计',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '22',
+    pid: '11',
+    title: '客服开发',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '23',
+    pid: '11',
+    title: '客服代表',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '24',
+    pid: '12',
+    title: '调度主管',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '26',
+    pid: '13',
+    title: '押运员',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+new QuestionCategory({
+    qcid: '27',
+    pid: '13',
+    title: '安全主任',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+
+new QuestionCategory({
+    qcid: '30',
+    pid: '24',
+    title: '调度助理',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+
+new QuestionCategory({
+    qcid: '31',
+    pid: '25',
+    title: '驾驶员',
+    isParent: true
+}).save(function (err) {
+        if (err) throw err;
+    });
+
+
+var qsid = 0;
+var qcids = ['10', '11', '12', '13', '20', '21', '23', '24', '25', '26', '27', '30', '31'];
+for (var i in qcids) {
+    //测试题目集
+    for(var g = 0; g < 5; g++){
+        var qs = new QuestionStore({
+            qsid: '' + (qsid++),
+            qcid: qcids[i],
+            title: '题目集_' + qsid,
+            drillScore: 2,
+            battleScore: 20,
+            maxTime: 1200,
+            bounty: 0,
+            papers: ''
+        });
+        qs.save(function (err) {
+            if (err) throw err;
+        });
     }
+}
 
-    for(var k = 0 ; k < 10; k++){
-
-        var qs = new QuestionStore();
-
-        qs.qsid = '' + k;
-        qs.qcid = '' + i;
-        qs.title = '题目集' + k;
-        qs.drillScore  = i+k;
-        qs.battleScore = i+k;
-        qs.maxTime = 1200;
-        qs.bounty = 0;
-        qs.papers = '';
-
-        qs.save(function(err){
-            if(err) throw err;
-        })
-
-        for(var g = 0; g < 10; g++){
-
-            var q  = new Question();
-
-            q.qid = '' + g;
-            q.qsid = '' + k;
-            q.title = '第' + g +  '题:XXXXXXXXXXXXXXXXXXX';
-            q.opts = {
-                a:'AAAAAAAAAAAAAAAAAAA',
-                b:'BBBBBBBBBBBBBBBBB',
-                c:'CCCCCCCCCCCCCCCCC'
-            };
-            q.answer = 'a';
+var qsid = 0;
+var qid = 0;
+for (var i in qcids) {
+    //测试题目
+    for(var g = 0; g < 5; g++){
+        qsid++;
+        for (var j = 0; j < 5; j++) {
+            var q = new Question({
+                qid: '' + (qid++),
+                qsid: '' + qsid,
+                title: '题目：把大象关进冰箱，需要几步?',
+                opts: {
+                    'A': '3步',
+                    'B': '2步',
+                    'C': '1步'
+                },
+                answer: 'A',
+                score: 1
+            });
             q.score = 1;
-            q.save(function(err){
-                if(err) throw err;
+            q.save(function (err) {
+                if (err) throw err;
+            });
+        }
+    }
+}
+
+var qsid = 0;
+var bid = 0;
+for (var i in qcids) {
+    //测试题目
+    for(var g = 0; g < 5; g++){
+        qsid++;
+        //测试挑战数据
+        for (var k = 0; k < 5; k++) {
+            var b = new Battle({
+                bid: '' + (bid++),
+                sid: '1',
+                qsid: '' + qsid,
+                qstitle : '题目集_' + qsid,
+                status: bid > 15 ? 'N' : bid > 10 ? 'F' : 'I',
+                drillScore: k,
+                battleScore: k * 5
+            });
+            b.save(function (err) {
+                if (err) throw err;
             })
         }
     }
 }
-*/
-
-for(var i = 0 ; i < 10; i++){
-
-    var b = new Battle();
-    b.bid = '' + i;
-    b.sid = '1';
-    b.qsid = '1';
-    b.qstitle = 'AAAAA题集';
-    b.status = i > 7 ? 'N' : i > 3 ? 'F' : 'I';
-
-    b.drillScore = 12;
-    b.battleScore = 100;
-
-    b.save(function(err){
-        if(err) throw err;
-    });
-}
-
-
-
