439db3f915dbdcb86b070055c9cb6504a90c4135
changhu <o52tiger@gmail.com>
2014-12-11 12:57:44 +0800

    修改设置

--- test_game/models/setting.js
+++ test_game/models/setting.js
@@ -1,32 +1,74 @@
-var mongoose = require('mongoose')
-    , Schema = mongoose.Schema
-    , ObjectId = Schema.ObjectId;
+/*
+ var mongoose = require('mongoose')
+ , Schema = mongoose.Schema
+ , ObjectId = Schema.ObjectId;
 
-var Setting = new Schema({
+ var Setting = new Schema({
 
-    //时间占比
-    timeScorePct : Number,
+ //时间占比
+ timeScorePct : Number,
 
-    //人数占比
-    userScorePct : Number,
+ //人数占比
+ userScorePct : Number,
 
-    //成功与否占比
-    succScorePct : Number,
+ //成功与否占比
+ succScorePct : Number,
 
-    //跳帧最多人数
-    maxUserNum : Number,
+ //跳帧最多人数
+ maxUserNum : Number,
 
-    //挑战最少人数
-    minUserNum : Number,
+ //挑战最少人数
+ minUserNum : Number,
 
-    //成功百分比
-    userSuccPct : Number,
+ //成功百分比
+ userSuccPct : Number,
 
-    //每个题集下生成的试卷的数量
-    paperNum: Number,
+ //每个题集下生成的试卷的数量
+ paperNum: Number,
 
-    //每个挑战题目数量
-    battleQuestionNum : Number
-});
+ //每个挑战题目数量
+ battleQuestionNum : Number
+ });
 
-mongoose.model('Setting', Setting, 'setting');
+ mongoose.model('Setting', Setting, 'setting');
+ */
+
+var fs = require('fs');
+
+var Setting = function (data) {
+    console.log(data);
+    this.data = data ;
+};
+
+var file =  __dirname + '\\config.json';
+fs.openSync(file, 'a');
+
+var doInit = function(){
+    var str = fs.readFileSync(file, 'UTF-8');
+    console.log(str);
+    return JSON.parse(str);
+};
+
+var doSave = function (data) {
+    var str = JSON.stringify(data);
+    fs.writeFileSync(file, str, 'UTF-8');
+};
+
+Setting.prototype.init = function(){
+    this.data = doInit();
+};
+
+Setting.prototype.save = function(){
+    doSave(this.data);
+};
+
+Setting.prototype.set = function(key, value){
+    this.data[key] = value;
+    this.save();
+};
+
+Setting.prototype.get = function(key){
+    return this.data[key];
+};
+
+module.exports = new Setting(doInit());
